51acf23a8b1fa0406c89f4ad09f30a21677a6ba5
klchg <71718565+klchg@users.noreply.github.com>
2022-09-05 00:11:44 -0400

    Update db_credentials.php

--- private/db_credentials.php
+++ private/db_credentials.php
@@ -1,8 +1,8 @@
 <?php
 
-define("DB_SERVER", "databasemysql.cedaccwzpawh.us-east-1.rds.amazonaws.com:3306");
-define("DB_USER", "admin");
-define("DB_PASS", "rOMGfDdZr4OwMDVGnUap");
-define("DB_NAME", "jz4364xl3760xj2026");
+define("DB_SERVER", "xxxxx");
+define("DB_USER", "xxxxx");
+define("DB_PASS", "xxxxxxxxxxxxx");
+define("DB_NAME", "xxxxxxxxxxxxxxxxxxx");
 
 ?>
